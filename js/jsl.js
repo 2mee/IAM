dadbe8e55ba9e95aa7e34d7254c2c175735abd02
2mee <malousar@th-brandenburg.de>
2019-05-03 03:04:16 +0200

    LE02 - event listeners

---
+++ js/jsl.js
@@ -0,0 +1,109 @@
+function initialiseView() {
+    // alert("initialiseView()");
+
+    const header = document.getElementsByTagName("header")[0];
+    const main = document.querySelector("main");
+    const view = header.querySelector("header .view"); //query selector reagiert auf css elemente
+    const refresh = document.querySelector("footer .refresh"); //query selector reagiert auf css elemente
+    const ul = main.getElementsByTagName("ul");
+    const add = header.querySelector("button");
+
+    // switching views
+
+    // header.onclick = () => {
+    //     // alert("onclick!");
+    //     // document.body.setAttribute("class", "tiles");
+    //     document.body.classList.toggle("tiles");
+    // };
+
+    // fade in - fade out
+
+    view.onclick = () => {
+        main.classList.toggle("faded");
+        setTimeout(() => {
+            document.body.classList.toggle("tiles");
+            main.classList.toggle("faded")
+        },2000);
+        main.addEventListener("transitionend", ontransitionend);
+    };
+
+
+    refresh.onclick = () => {
+        main.classList.toggle("faded");
+        // *** ertste Möglichkeit ***
+        // setTimeout(() =>main.classList.toggle("faded"),2000);
+
+        // *** zweite Möglichkeit ***
+        setTimeout(() => {
+            main.classList.toggle("faded")
+            },2000);
+
+        // *** dritte Möglichkeit ***
+        // setTimeout(() => {
+        //     main.classList.toggle("faded")},5000);
+        main.addEventListener("transitionend", ontransitionend);
+    };
+
+    function ontransitionend() {
+        main.classList.toggle("faded");
+        main.removeEventListener("transionenend", ontransitionend);
+    }
+
+    // list items selection
+    // var litems = main.getElementsByTagName("li");
+    // for (var i = 0; i < litems.length; i++) {
+    //     let currentli = litems[i];
+    //     currentli.onclick = (evt) => {
+    //         alert("Select:" + getLiTitle(currentli) + "click on: " + evt.target);
+            // Event target evt.target gibt an auf welchem Element das Event (z.B. der click) tatsächlich stattgefunden hat
+    //     }
+    // }
+
+    // var litems = main.querySelectorAll("li");
+    // litems.forEach(currentli => {
+    //     currentli.onclick = (evt) => {
+    //             alert("Select:" + getLiTitle(currentli) + "click on: " + evt.target);
+    //         }
+    // });
+
+    ul.onclick = (evt) => {
+        var currentli = getCurrentli(evt.target);
+        // evt.target.classList ....
+        alert("Selected: " + getLiTitle(currentli) +  ", click event on: " + evt.target);
+    }
+
+    function getCurrentli(el) {
+        if (el.tagName == "LI") {
+            return el;
+        }
+        else if (el.tagName == "UL") {
+            alert("Reached list root, and no LI has been found!")
+        }
+        else if (el.parentNode) {
+            return getCurrentli(el.parentNode);
+        }
+        else {
+            alert("Something went completely wrong!");
+        }
+    }
+
+    function getLiTitle(li) {
+        return li.getElementsByClassName("title_name")[0].textContent;
+    }
+
+    // add new elements
+    add.onclick = () => {
+        evt.stopPropagation(); // unterbindet das Wandern durch den Baum
+        addLiElementToList({title: "New Element:" + Date.now(),src:"http://..." });
+       // alert("add new element");
+    }
+
+    function addLiElementToList(obj){
+        // alert("add new element for: " + JSON.stringify(obj));
+        console.log("add new element for: " + JSON.stringify(obj));
+        ul.innerHTML = ul.innerHTML + "<li><img alt class=\'align-left'\ src=\"" + obj.src + "\"/><h2>" + obj.title + "</h2> </li>";
+
+    }
+}
+
+window.onload = initialiseView;
